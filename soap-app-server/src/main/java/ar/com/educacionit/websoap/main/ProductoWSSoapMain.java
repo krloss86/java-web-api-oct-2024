c0ad3f5533f895da8d722fdb4c2fc1af19a370dc
carlos horacio lopez <carloshoracio.lopez@comafi.com.ar>
2024-11-09 00:17:48 -0300

    clase2: add soap server

--- soap-app-server/src/main/java/ar/com/educacionit/websoap/main/ProductoWSSoapMain.java
+++ soap-app-server/src/main/java/ar/com/educacionit/websoap/main/ProductoWSSoapMain.java
@@ -6,6 +6,12 @@ import jakarta.xml.ws.Endpoint;
 public class ProductoWSSoapMain {
 
 	public static void main(String[] args) {
+		System.out.println("Publicando Servicio Web de producto...");
+
 		Endpoint.publish("http://localhost:8000/", new ProductoeWsSoapServiceImpl());
+		
+		System.out.println("Servicio Web de producto iniciado en http://localhost:8000/");
+		
+		System.out.println("La direccion del wsdl es: http://localhost:8000/?wsdl");
 	}
 }
